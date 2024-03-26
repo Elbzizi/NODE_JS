1983919e9862a067894f350b9a66823264a4e9bc
omar <omar@tzst.com>
2024-03-26 22:49:58 +0100

    node js Event

---
+++ Functions/Event.js
@@ -0,0 +1,21 @@
+function print() {
+    console.log("hello world");
+  }
+  
+  var prant = function print() {
+    console.log("hello world");
+  };
+  
+  //katrdam rasha ===
+  (function print() {
+    console.log("hello world last ");
+  })();
+  
+  var print = function () {
+    console.log("hello world");
+  };
+  
+  var print = () => {
+    console.log("hello world");
+  };
+  
