c95c4a21d76e76a6618cab363b2abfe7da88b65c
omar <omar@tzst.com>
2024-03-30 22:50:43 +0100

    client side project notaty

--- notaty/client/note-client.js
+++ notaty/client/note-client.js
@@ -3,16 +3,22 @@ baseUrl = "http://localhost:3000";
 async function addNote(data) {
   const response = await fetch(`${baseUrl}/notes`, {
     method: "POST",
-    headers: {"Content-Type":"application/json"},
-    body:JSON.stringify(data)
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
   });
   return response;
 }
 async function UpdateNote(data) {
-    const response = await fetch(`${baseUrl}/notes`, {
-      method: "PUT",
-      headers: {"Content-Type":"application/json"},
-      body:JSON.stringify(data)
-    });
-    return response;
-  }
+  const response = await fetch(`${baseUrl}/notes`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  return response;
+}
+async function DleteNote(id) {
+  const response = await fetch(`${baseUrl}/notes/${id}`, {
+    method: "DELETE",
+  });
+  return response;
+}
