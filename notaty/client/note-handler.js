b006d55e7c152427d0e9575056f7bb1f6153d5e3
omar <omar@tzst.com>
2024-03-31 23:25:16 +0200

    client side project notaty

--- notaty/client/note-handler.js
+++ notaty/client/note-handler.js
@@ -15,6 +15,14 @@ function updateNotesTable() {
       // tr.appendChild(td3);
       // tr.appendChild(td4);
       // table.appendChild(tr);
+      row = table.insertRow(1);
+      cell1 = row.insertCell(0);
+      cell2 = row.insertCell(1);
+      cell3 = row.insertCell(2);
+      cell4 = row.insertCell(3);
+      cell1.textContent = ele["title"];
+      cell2.textContent = ele["content"];
+      cell3.textContent = ele["updatedDate"];
     });
   });
 }
